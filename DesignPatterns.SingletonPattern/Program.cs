0269eb950809164590b887ccf63786f5bf045060
Yakup Buğra Bozkurt <yakup_bugra@hotmail.com>
2016-12-03 21:54:19 +0300

    Add project files.

---
+++ DesignPatterns.SingletonPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DesignPatterns.SingletonPattern
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var singleton1 = Singleton.CreateSingletonObject();
+            var singleton2 = Singleton.CreateSingletonObject();
+            var singleton3 = Singleton.CreateSingletonObject();
+
+            singleton1.MyProperty = "Hello World!";
+
+            Console.WriteLine(singleton1.MyProperty);
+            Console.WriteLine(singleton2.MyProperty);
+            Console.WriteLine(singleton3.MyProperty);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DesignPatterns.SingletonPattern/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DesignPatterns.SingletonPattern
+{
+    public class Singleton
+    {
+        private static volatile Singleton single;
+        private static Object lock_obj = new object();
+        public string MyProperty { get; set; }
+
+        protected Singleton()
+        {
+        }
+
+        public static Singleton CreateSingletonObject()
+        {
+            lock (lock_obj)
+            {
+                if (single == null)
+                {
+                    single = new Singleton();
+                }
+                return single;
+            }
+        }
+    }
+}
